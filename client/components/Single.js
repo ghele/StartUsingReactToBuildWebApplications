12b4f8a83b254e3ad93ba9caca1f676d347b69a2
ghele <gheletus_raoul@yahoo.com>
2016-11-29 09:06:03 +0200

    Displaying the single photo component (and comment prototype)

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass( {
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex( (post) => post.code === this.props.params.postId );
+    const post = this.props.posts[i];
+    console.log(post);
+    // get us the post
     return (
-      <div className="single-photo">I am the single</div>
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
     );
   }
 } );
